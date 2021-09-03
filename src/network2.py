c1828d253aceae0e5f0ab564950efe85646a815b
jayphone17 <m18026629951@163.com>
2021-09-03 22:22:25 +0800

    更新network2

--- src/network2.py
+++ src/network2.py
@@ -37,7 +37,7 @@ class Network(object):
 
   def __init__(self,sizes,cost = CrossEntropyCost):
     self.num_layer = len(sizes)
-    self.siezes = sizes
+    self.sizes = sizes
     self.default_weight_initializer()
     self.cost = cost
 
@@ -50,7 +50,7 @@ class Network(object):
                     for x,y in zip(self.sizes[:-1], self.sizes[1:])]
 
   def large_weight_initializer(self):
-    self.biases = [np.random.randn(y,1) for y in self.sieze[1:]]
+    self.biases = [np.random.randn(y,1) for y in self.sizes[1:]]
     self.weights = [np.random.randn(y,x)
                     for x,y in zip(self.sizes[:-1], self.sizes[1:])]
 
